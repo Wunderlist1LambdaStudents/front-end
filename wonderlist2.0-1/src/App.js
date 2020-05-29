0aaee07720127f90577eadf6e133eb414ecff085
Jeffrey Valdez <53051337+jvaldezdipre@users.noreply.github.com>
2020-05-29 15:08:09 -0400

    idk

--- wonderlist2.0-1/src/App.js
+++ wonderlist2.0-1/src/App.js
@@ -4,41 +4,44 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import { Route, Switch } from 'react-router-dom';
-import { CssBaseline, createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+import {
+  CssBaseline,
+  createMuiTheme,
+  MuiThemeProvider,
+} from '@material-ui/core';
 import Profile from './components/Profile';
 import PrivateRoutes from './components/PrivateRoutes';
 import NavBar from './components/NavBar';
 
 const theme = createMuiTheme({
-  palette:{
-    common:{
-      white:"#fafafa"
-
+  palette: {
+    common: {
+      white: '#fafafa',
     },
-    primary:{
-      main:'#1e88e5ff'
+    primary: {
+      main: '#1e88e5ff',
       //light:''
       //dark:''
     },
     //secondary:{
-      //main:#
+    //main:#
     //},
-    error:{
-      main:'#ff0c3eff'
+    error: {
+      main: '#ff0c3eff',
     },
-    warning:{
-      main:'#FFC107'
+    warning: {
+      main: '#FFC107',
     },
     //info:{
-      //main:'#'
+    //main:'#'
     //},
-    success:{
-      main:'#27AE60'
-    }
-  }
-})
+    success: {
+      main: '#27AE60',
+    },
+  },
+});
 
-console.log(theme)
+console.log(theme);
 
 function App() {
   return (
@@ -46,9 +49,9 @@ function App() {
       <MuiThemeProvider theme={theme}>
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/signup' component={Signup} />
-          <Route path='/login' component={Login} />
-          <PrivateRoutes path='/profile' component={Profile} />
+          <Route path='/signup' exact component={Signup} />
+          <Route path='/login' exact component={Login} />
+          <PrivateRoutes path='/profile' exact component={Profile} />
         </Switch>
       </MuiThemeProvider>
     </CssBaseline>
