226c48b06896e412ad1a8068a0d8625b0cb69f4c
Jeffrey Valdez <53051337+jvaldezdipre@users.noreply.github.com>
2020-05-29 14:49:52 -0400

    fixed sign up look ugly but works i guess

--- wonderlist2.0-1/src/components/Signup.js
+++ wonderlist2.0-1/src/components/Signup.js
@@ -1,18 +1,66 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+
+import { signupSubmitHandler } from '../store/actions/userActions';
+
 import MyForm from './formComponents/MyForm';
 import { signUpFormData, formStyle } from './formComponents/formComponentsData';
+
+import { Button, Container } from '@material-ui/core';
+import {
+  InputLabel,
+  Input,
+  FormControl,
+  FormHelperText,
+} from '@material-ui/core';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 
-const Signup = () => {
+const Signup = ({ signupSubmitHandler }) => {
+  const history = useHistory();
+  const { register, handleSubmit, errors, watch } = useForm({ mode: 'onBlur' });
+
+  const onSubmit = data => {
+    console.log(data);
+    signupSubmitHandler(data);
+    history.push('/');
+  };
+
   return (
     //Michael - Temporary Styling
     <div className='form-flex'>
       <div className='check-logo'>
         <CheckCircleIcon fontSize='large' color='primary' />
       </div>
-      <MyForm data={signUpFormData} style={formStyle} />
+      {/* <MyForm data={signUpFormData} style={formStyle} /> */}
+      {/* <FormControl onSubmit={handleSubmit(onSubmit)}>
+        <InputLabel htmlFor='username'></InputLabel>
+        <Input name='username' type='text' inputRef={register} />
+        <InputLabel htmlFor='password'></InputLabel>
+        <Input name='password' type='password' inputRef={register} />
+        {/* <Button variant='outlined' type='submit'>
+          Sign up
+        </Button> */}
+      {/* <button>sign up</button> */}
+      {/* <Button type='button' variant='outlined'>
+          Go Back
+        </Button> */}
+      {/* </FormControl> */}
+      <form action='' onSubmit={handleSubmit(onSubmit)}>
+        <input type='text' name='username' ref={register} />
+        <input type='password' name='password' ref={register} />
+        <button> sign up</button>
+        <button type='button' onClick={() => history.push('/')}>
+          Go back
+        </button>
+      </form>
     </div>
   );
 };
 
-export default Signup;
+const mapStateToProps = state => {
+  return state;
+};
+
+export default connect(null, { signupSubmitHandler })(Signup);
